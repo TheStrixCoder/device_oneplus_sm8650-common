bff421a16a30630fc2d2aa459e27fc86e32765cf
Pranav Vashi <neobuddy89@gmail.com>
2023-05-07 19:05:57 +0530

    sm8650-common: init: Reset readahead values for 128 always

--- init/init.qcom.post_boot.sh
+++ init/init.qcom.post_boot.sh
@@ -630,10 +630,18 @@ function configure_zram_parameters() {
 }
 
 function configure_read_ahead_kb_values() {
-
-    dmpts=$(ls /sys/block/*/queue/read_ahead_kb | grep -e dm -e mmc)
-
+	dmpts=$(ls /sys/block/*/queue/read_ahead_kb | grep -e dm -e mmc)
 	ra_kb=128
+
+	if [ -f /sys/block/mmcblk0/bdi/read_ahead_kb ]; then
+		echo $ra_kb > /sys/block/mmcblk0/bdi/read_ahead_kb
+	fi
+	if [ -f /sys/block/mmcblk0rpmb/bdi/read_ahead_kb ]; then
+		echo $ra_kb > /sys/block/mmcblk0rpmb/bdi/read_ahead_kb
+	fi
+	for dm in $dmpts; do
+		echo $ra_kb > $dm
+	done
 }
 
 function disable_core_ctl() {
