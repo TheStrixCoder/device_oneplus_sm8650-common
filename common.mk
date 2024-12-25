9df01e556ef623f5573c862a78ea8f2cd9d3d21a
LuK1337 <priv.luk@gmail.com>
2024-12-25 23:42:18 +0100

    sm8650-common: Switch to NCM USB tethering
    Change-Id: I20b6b2b2e26a45d8b28c7483afc83ad4da4b8643
    (cherry picked from commit 9d876285052b3b3c4aeef5a081743c2111b0c191)

--- common.mk
+++ common.mk
@@ -416,6 +416,7 @@ PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_PACKAGES += \
     CarrierConfigResCommon \
     FrameworksResTarget \
+    NcmTetheringOverlay \
     OPlusFrameworksResCommon \
     OPlusSettingsResCommon \
     OPlusSystemUIResCommon \
